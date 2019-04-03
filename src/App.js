3f6f1bdb2dcc7673ec0901950cac7e93fc96c8ad
tacoder <chris.pylinski@googlemail.com>
2019-04-03 22:44:49 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import { Route } from "react-router-dom";
 import Home from "./Home";
 import Profile from "./Profile";
 import Nav from "./Nav";
+import Auth from "./Auth/Auth";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.auth = new Auth(this.props.history); // can also be put in state
+  }
   render() {
     return (
       // this is short for <React.Fragment> <> </> babel can compile down
       <>
         <Nav />
         <div className="body">
-          <Route path="/" exact component={Home} />
+          <Route
+            path="/"
+            exact
+            render={props => <Home auth={this.auth} {...props} />}
+          />
           <Route path="/profile" component={Profile} />
         </div>
       </>
--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -5,11 +5,15 @@ export default class Auth {
   constructor(history) {
     this.history = history;
     this.auth0 = new auth0.WebAuth({
-      domain: process.env.REACT_APP_AUTH0_DOMAIN,
+      //domain: process.env.REACT_APP_AUTH0_DOMAIN,
       clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
       redirectUri: process.env.REACT_APP_AUTH0_REDIRECT_CALLBACK_URL,
       responseType: "token id_token",
       scope: "openid profile email" // scope where permissions are specified openid for authentication
     });
   }
+  // class property syntax so no this.bind
+  login = () => {
+    this.auth0.authorize(); // will redirect the browser to auth0
+  };
 }
